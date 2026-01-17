aa2adc8fdfb288b8b0f133584c17ba85506d488b
Harjot Grewal <harjotgrewal@Harjots-MacBook-Pro.local>
2026-01-16 18:49:51 -0800

    Start

---
+++ NFA.py
@@ -0,0 +1,14 @@
+
+class NFA: 
+    def __init__(self,states,alphabet,transition_function,start_state,accept_states):
+        self.states = states
+        self.alphabet = alphabet
+        self.transiton_function = transition_function
+        self.start_state = start_state
+        self.accept_states = accept_states
+
+
+
+
+
+        
